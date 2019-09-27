d4ab35b1d86a5665de3a9724a1d586586ca95704
sthephmaldonado <54649984+sthephmaldonado@users.noreply.github.com>
2019-09-27 19:05:37 -0400

    Add files via upload

---
+++ Wek2&3Excercice6.4.py
@@ -0,0 +1,16 @@
+#%%
+#
+# keyword : def 
+def add(value1,value2,value3):
+    return value1 + value2 + value3
+    
+
+
+#%%
+# keyword : def
+Value1= 4
+Value2= 2
+Value3= 10 
+print(add(4,2,10))
+
+#%%
