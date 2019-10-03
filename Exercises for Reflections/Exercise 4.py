a267f831a119660951dff62697ba101bfe3d05cc
danielleaneal <54646998+danielleaneal@users.noreply.github.com>
2019-10-02 21:49:57 -0400

    Collaborating with you!

--- Exercises for Reflections/Exercise 4.py
+++ Exercises for Reflections/Exercise 4.py
@@ -4,3 +4,4 @@
 print("Hello World. It's Sthephany.") 
 
 #%%
+print("Hello Sthephany's world! It's Danielle, attempting to collaborate with Sthephany. Will I be able to commit these changes? That is the question.")
