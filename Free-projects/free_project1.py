988aaa212abf3e795cdeb27fdbaa06c8372b52b0
sthephmaldonado <smaldonado1@knights.ucf.edu>
2019-10-16 19:16:01 -0400

    deleting spaces

--- Free-projects/free_project1.py
+++ Free-projects/free_project1.py
@@ -1,21 +1,21 @@
 from random import choice
 
-subjects = ['COUNT', 'STRANGER', 'LOOK', 'CHURCH', 'CASTLE', 'PICTURE',
-            'EYE', 'VILLAGE', 'TOWER', 'FARMER', 'WAY', 'GUEST', 'DAY',
+subjects = ['NUMBER', 'PERSON', 'LOOK', 'CHURCH', 'CASTLE', 'FRAME',
+            'APPLE', 'VILLAGE', 'BUILDING', 'FARMER', 'WAY', 'GUEST', 'WEEK',
             'HOUSE', 'TABLE', 'LABOURER']
 predicates = ['CLOSED', 'SILENT', 'STRONG', 'GOOD', 'NARROW', 'NEAR',
-              'NEW', 'QUIET', 'FAR', 'DEEP', 'LATE', 'DARK', 'FREE',
+              'NEW', 'NOISE', 'CLOSE', 'DEEP', 'EARLY', 'DARK', 'FREE',
               'LARGE', 'OLD', 'ANGRY']
 conjunctions = [' AND ', ' OR ', ' THEREFORE ', ' BUT ALSO ', 'WHETHER ', ' NOR ', ' BUT ', ' IF ']
 operators = ['A', 'EVERY', 'NO', 'NOT EVERY']
 
-def phrase(subjects):
-    text = choice(operators) + ' predicates ' + choice(subjects)
-    if text == 'A EYE':
-        text = 'AN EYE'
+def phrase( subjects ):
+    text = choice (operators) + '' + choice (subjects)
+    if text == 'A APPLE':
+        text = 'AN APPLE'
     return text + 'IS'
 
 print('')
-print(phrase(subjects) + choice(predicates) + choice(conjunctions) +
-       phrase(operators) + choice(predicates) + '.')
+print( phrase (subjects) + choice (predicates) + choice (conjunctions) +
+       phrase (operators) + choice (predicates) + '.')
 print('')
