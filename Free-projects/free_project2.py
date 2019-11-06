bb5e82b34e0b768b047c1e96f22b3063fd541b38
sthephmaldonado <54649984+sthephmaldonado@users.noreply.github.com>
2019-11-06 17:59:17 -0500

    add image and rotate

--- Free-projects/free_project2.py
+++ Free-projects/free_project2.py
@@ -1,18 +1,33 @@
-#%%
-from PIL import Image
- 
-img = Image.new('RGB', (60, 30), color = 'blue')
-
-img
 
 #%%
 from PIL import Image, ImageDraw
  
-img = Image.new('RGB', (100, 60), color = ('pink'))
+img = Image.new('RGB', (200, 200), color = ('pink'))
  
 d = ImageDraw.Draw(img)
-d.text((10,10), "Sthephany", fill=(255,255,0))
+d.text((50,50), "Sthephany", fill=('black'))
  
-img.save('pil_text.png')
-
 img
+
+#%%
+ from PIL import Image, ImageFilter 
+ 
+ img = Image.new('RGB', (200, 200), color = ('pink'))
+ img.new(ImageFilter.GaussianBlur(15)).save
+
+ img 
+
+###Trying to make the image blur but isnt working. https://stackoverflow.com/questions/21215903/blurring-an-image-using-pil-in-python
+
+#%%
+from PIL import Image
+
+img = Image.open ("Free-projects/beach1.jpg")
+img 
+
+#%%
+from PIL import Image
+
+img = Image.open ("Free-projects/beach1.jpg")
+img.rotate(180).show()
+
